5d056ad7173653e3dca0b349b2e2ee05e502b894
Tkhintkiria <khintkiriatako@gmail.com>
2025-07-03 18:51:25 +0400

    Create HomeworkJS.js

---
+++ HomeworkJS.js
@@ -0,0 +1,42 @@
+var a = 65
+var b = 87
+console.log(a+b)
+
+let Student = { firstName:'tamari', lastName: 'Khintkiria', age: 33, email:'xxx@gmail.com', marks: [3,4,5]}
+console.log(Student.firstName)
+
+let m = 45;
+let n = 23;
+
+if (m > n) {
+  console.log( m + n);
+} else {
+  console.log( n);
+}
+
+let Studentname="Tamari"
+console.log("Raise and shine", Studentname)
+
+let bonus = 0
+let salary = 5000
+if (salary>=2000) {
+    bonus=salary*0.1
+}
+else if(salary<2000){
+    bonus=0
+}
+console.log(bonus)
+
+var a = 23
+var b = a/2
+var c = a+b
+console.log(c)
+
+let Phones = ['Iphone', 'Samsung', 'Pixel', 'Huawei', 'Xiaomi']
+console.log(Phones)
+
+let Phones = ['Iphone', 'Samsung', 'Pixel', 'Huawei', 'Xiaomi']
+console.log(Phones[0])
+console.log(Phones[Phones.length - 1])
+
+
